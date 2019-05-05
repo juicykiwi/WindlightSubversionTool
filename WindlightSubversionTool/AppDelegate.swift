b316ef77ac734dde2abc352f299d332a066f5884
Juicy <joen0622@gmail.com>
2019-05-05 19:32:34 +0900

    Windlight subversion tool init

---
+++ WindlightSubversionTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WindlightSubversionTool
+//
+//  Created by Juicy on 2019. 5. 5..
+//  Copyright © 2019년 Juicy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WindlightSubversionTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WindlightSubversionTool
+//
+//  Created by Juicy on 2019. 5. 5..
+//  Copyright © 2019년 Juicy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
